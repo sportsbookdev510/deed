1d6d8c2b8d4ff7c48ebd26120cd7a5aa4104f21b
sportsbookdev510 <kaiserman1029@gmail.com>
2026-09-19 12:03:07 +0900

    .

---
+++ _next/static/chunks/app/page-c49c082d82d1c010.js
@@ -0,0 +1,363 @@
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+  [974],
+  {
+    185: (L, e, a) => {
+      Promise.resolve().then(a.bind(a, 3448));
+    },
+    3448: (L, e, a) => {
+      "use strict";
+      a.d(e, { default: () => m });
+      var s = a(5155),
+        l = a(2115);
+      let t = [
+        "M0,84.3L163.6,0L326.6,84.5L326.6,124L163.8,63.5L0,124.5Z",
+        "M0,150L163.8,94L326.6,150.5L326.6,199L163.8,161.5L0,198.8Z",
+        "M0.5,224.3L164,188L326.6,224.8L326.6,267L164,248.2L0.5,266.5Z",
+        "M0.5,293L163,276.5L326.6,293L326.6,328.5L0.5,328.8Z",
+      ];
+      function i() {
+        return (0, s.jsxs)("svg", {
+          viewBox: "0 0 1226 330",
+          fill: "currentColor",
+          "aria-hidden": "true",
+          children: [
+            t.map((L) => (0, s.jsx)("path", { d: L }, L)),
+            (0, s.jsx)("path", {
+              fillRule: "evenodd",
+              d: "M1006.0,105.5L1006.0,311.8L1006.5,312.2L1111.2,312.0L1127.8,310.8L1139.2,308.8L1147.5,306.8L1159.5,302.8L1164.2,300.8L1165.5,299.8L1168.5,298.8L1176.5,294.5L1182.5,290.8L1192.0,283.5L1201.5,274.2L1204.5,270.8L1209.8,263.5L1214.8,255.0L1218.8,245.8L1221.8,236.5L1223.0,229.5L1223.5,228.8L1224.8,219.2L1224.8,197.5L1222.5,183.5L1219.8,174.0L1216.8,166.5L1212.8,159.2L1212.8,158.5L1206.8,149.5L1201.5,143.0L1193.5,135.0L1187.8,130.2L1177.0,123.0L1165.2,117.0L1164.5,117.0L1163.2,116.0L1156.5,113.8L1155.5,113.0L1145.5,110.0L1131.2,107.0L1123.2,106.0L1106.8,105.0L1006.5,105.0ZM1052.5,147.0L1053.8,146.2L1114.8,146.5L1125.5,147.8L1137.0,150.8L1144.2,153.8L1154.8,160.0L1159.5,164.0L1166.2,171.5L1172.0,180.5L1175.8,190.8L1177.0,196.5L1177.8,203.0L1177.8,214.5L1176.8,222.0L1175.0,227.8L1175.0,229.0L1173.0,234.2L1170.0,240.2L1165.8,246.5L1162.2,250.5L1155.0,257.0L1147.2,262.0L1135.5,267.0L1128.0,269.0L1112.2,271.0L1053.2,271.0L1052.5,270.2ZM812.2,105.0L812.0,106.0L814.0,116.5L819.0,151.2L820.0,155.2L821.0,164.2L823.0,174.5L823.0,177.2L825.0,187.5L825.0,190.2L827.0,200.5L828.0,210.0L825.0,226.8L824.8,230.8L823.0,239.8L822.8,243.8L821.0,252.8L820.8,256.8L819.0,265.8L819.0,268.0L818.2,270.8L817.0,281.0L816.2,283.5L815.0,294.0L814.2,296.5L813.0,307.0L812.0,311.5L998.0,311.8L998.2,273.8L997.8,273.2L863.2,273.2L862.8,272.8L863.8,264.8L864.8,261.0L865.0,257.0L866.5,249.8L866.8,245.8L868.8,235.2L869.0,231.5L869.5,230.5L869.8,227.2L870.5,226.2L989.2,226.2L989.8,225.5L989.8,191.2L989.2,190.8L870.2,190.5L869.8,189.5L868.8,181.5L867.8,177.5L867.8,175.2L866.8,171.2L866.8,168.5L865.8,164.5L862.8,144.2L863.2,143.8L996.5,143.8L997.0,143.0L996.8,105.2ZM616.2,105.0L616.0,107.2L617.0,111.2L618.0,120.2L620.0,130.5L620.0,133.2L622.0,143.5L622.0,146.2L623.0,150.2L623.0,152.5L624.0,156.5L624.2,160.5L625.0,163.2L625.0,165.2L626.0,169.5L627.0,178.2L632.0,208.8L631.0,212.8L631.0,215.0L629.2,224.0L629.0,228.0L628.2,230.5L627.0,241.0L626.2,243.5L625.0,254.0L622.0,270.8L621.8,274.8L620.0,283.8L619.8,287.8L618.0,296.8L618.0,299.0L616.0,311.5L616.8,312.0L803.5,311.8L804.0,311.2L806.0,299.0L806.5,298.2L807.8,289.8L808.5,287.8L808.8,284.5L810.8,275.5L810.8,274.0L810.0,273.5L773.0,273.8L667.2,273.2L666.5,272.2L669.8,253.2L669.8,251.0L670.8,247.0L671.0,243.0L672.5,235.8L672.8,231.8L674.2,226.2L812.8,226.2L813.8,225.0L813.8,223.8L817.8,209.0L813.5,191.2L813.0,190.8L674.5,190.8L673.8,190.0L672.8,185.2L672.8,182.5L671.8,178.5L671.8,176.5L669.8,166.0L669.8,163.5L669.0,161.0L668.8,157.2L666.5,144.5L667.2,143.8L809.8,143.8L810.2,143.2L809.8,142.0L809.8,139.8L808.8,136.0L808.8,133.8L807.8,130.0L807.8,127.8L806.8,124.0L806.8,121.8L805.8,118.0L805.8,115.8L803.8,105.2ZM401.0,105.5L401.0,311.8L401.5,312.2L506.2,312.0L522.8,310.8L534.2,308.8L542.5,306.8L554.5,302.8L559.2,300.8L560.5,299.8L563.5,298.8L571.5,294.5L577.5,290.8L587.0,283.5L596.5,274.2L599.5,270.8L604.8,263.5L609.8,255.0L613.8,245.8L616.8,236.5L618.0,229.5L618.5,228.8L619.8,219.2L619.8,197.5L617.5,183.5L614.8,174.0L611.8,166.5L607.8,159.2L607.8,158.5L601.8,149.5L596.5,143.0L588.5,135.0L582.8,130.2L572.0,123.0L560.2,117.0L559.5,117.0L558.2,116.0L551.5,113.8L550.5,113.0L540.5,110.0L526.2,107.0L518.2,106.0L501.8,105.0L401.5,105.0ZM447.5,147.0L448.8,146.2L509.8,146.5L520.5,147.8L532.0,150.8L539.2,153.8L549.8,160.0L554.5,164.0L561.2,171.5L567.0,180.5L570.8,190.8L572.0,196.5L572.8,203.0L572.8,214.5L571.8,222.0L570.0,227.8L570.0,229.0L568.0,234.2L565.0,240.2L560.8,246.5L557.2,250.5L550.0,257.0L542.2,262.0L530.5,267.0L523.0,269.0L507.2,271.0L448.2,271.0L447.5,270.2Z",
+            }),
+          ],
+        });
+      }
+      function r() {
+        return (0, s.jsx)("svg", {
+          viewBox: "0 0 327 329",
+          "aria-hidden": "true",
+          children: t.map((L) => (0, s.jsx)("path", { d: L }, L)),
+        });
+      }
+      let n = () =>
+          (0, s.jsx)("svg", {
+            viewBox: "0 0 256 256",
+            fill: "currentColor",
+            "aria-hidden": "true",
+            children: (0, s.jsx)("path", {
+              d: "M221.66,133.66l-72,72a8,8,0,0,1-11.32-11.32L196.69,136H40a8,8,0,0,1,0-16H196.69L138.34,61.66a8,8,0,0,1,11.32-11.32l72,72A8,8,0,0,1,221.66,133.66Z",
+            }),
+          }),
+        o = () =>
+          (0, s.jsx)("svg", {
+            viewBox: "0 0 256 256",
+            fill: "currentColor",
+            "aria-hidden": "true",
+            children: (0, s.jsx)("path", {
+              d: "M228.88,26.19a9,9,0,0,0-9.16-1.57L17.06,103.93a14.22,14.22,0,0,0,2.43,27.21L72,141.45V200a15.92,15.92,0,0,0,10,14.83,15.91,15.91,0,0,0,17.51-3.73l25.32-26.26L165,220a15.88,15.88,0,0,0,10.51,4,16.3,16.3,0,0,0,5-.79,15.85,15.85,0,0,0,10.67-11.63L231.77,35A9,9,0,0,0,228.88,26.19Zm-61.14,36L78.15,126.35l-49.6-9.73ZM88,200V152.52l24.79,21.74Zm87.53,8L92.85,135.5l119-85.29Z",
+            }),
+          }),
+        c = () =>
+          (0, s.jsx)("svg", {
+            viewBox: "0 0 256 256",
+            fill: "currentColor",
+            "aria-hidden": "true",
+            children: (0, s.jsx)("path", {
+              d: "M214.75,211.71l-62.6-98.38,61.77-67.95a8,8,0,0,0-11.84-10.76L143.24,99.34,102.75,35.71A8,8,0,0,0,96,32H48a8,8,0,0,0-6.75,12.3l62.6,98.37-61.77,68a8,8,0,1,0,11.84,10.76l58.84-64.72,40.49,63.63A8,8,0,0,0,160,224h48a8,8,0,0,0,6.75-12.29ZM164.39,208,62.57,48h29L193.43,208Z",
+            }),
+          }),
+        d = "deed-waitlist",
+        h = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+      function m(L) {
+        let { xUrl: e = "https://x.com/deedestate_rh", telegramFallback: a = "" } =
+            L,
+          [t, m] = (0, l.useState)(!1),
+          [u, x] = (0, l.useState)(""),
+          [p, j] = (0, l.useState)(null),
+          [g, f] = (0, l.useState)(!1),
+          [N, b] = (0, l.useState)(null),
+          v = (0, l.useRef)(null),
+          y = (0, l.useRef)(null);
+        (0, l.useEffect)(() => {
+          let L = !1,
+            e = (L) =>
+              new Promise((e) => {
+                let a = new Image();
+                (a.onload = a.onerror = () => e()), (a.src = L);
+              }),
+            a = new Promise((L) => setTimeout(L, 900)),
+            s = new Promise((L) => setTimeout(L, 3500));
+          return (
+            Promise.race([
+              Promise.all([e("/top-bg.png"), e("/bottom-bg.png"), a]),
+              s,
+            ]).then(() => {
+              L || m(!0);
+            }),
+            () => {
+              L = !0;
+            }
+          );
+        }, []),
+          (0, l.useEffect)(() => {
+            try {
+              let L = JSON.parse(localStorage.getItem(d) || "null");
+              L && L.email && b({ email: L.email, tg: L.tg });
+            } catch (L) {}
+          }, []);
+        let w = async (L) => {
+            var e, a, s;
+            L.preventDefault();
+            let l = u.trim().toLowerCase();
+            if (!h.test(l)) {
+              j("Enter a valid email address."),
+                null == (e = v.current) || e.focus();
+              return;
+            }
+            f(!0);
+            try {
+              let L = await fetch("/api/waitlist", {
+                  method: "POST",
+                  headers: { "Content-Type": "application/json" },
+                  body: JSON.stringify({
+                    email: l,
+                    company: (null == (a = y.current) ? void 0 : a.value) || "",
+                    ref: document.referrer || "",
+                  }),
+                }),
+                e = await L.json().catch(() => ({}));
+              if (!L.ok || !e.ok)
+                throw Error(
+                  e.error || "Could not add you. Try again in a moment."
+                );
+              try {
+                localStorage.setItem(
+                  d,
+                  JSON.stringify({ email: l, tg: e.telegram || "" })
+                );
+              } catch (L) {}
+              b({ email: l, tg: e.telegram });
+            } catch (e) {
+              let L = e instanceof Error ? e.message : "";
+              j("Failed to fetch" === L ? "Connection failed. Try again." : L),
+                null == (s = v.current) || s.focus();
+            } finally {
+              f(!1);
+            }
+          },
+          E = (null == N ? void 0 : N.tg) || a;
+        return (0, s.jsxs)(s.Fragment, {
+          children: [
+            (0, s.jsx)("div", {
+              className: "splash".concat(t ? " out" : ""),
+              "aria-hidden": "true",
+              children: (0, s.jsx)(r, {}),
+            }),
+            (0, s.jsx)("img", {
+              className: "bg bg-top".concat(t ? " in" : ""),
+              src: "/top-bg.png",
+              alt: "",
+              width: 2400,
+              height: 800,
+              fetchPriority: "high",
+            }),
+            (0, s.jsx)("img", {
+              className: "bg bg-bottom".concat(t ? " in" : ""),
+              src: "/bottom-bg.png",
+              alt: "",
+              width: 2400,
+              height: 800,
+              fetchPriority: "high",
+            }),
+            (0, s.jsxs)("div", {
+              className: "page".concat(t ? " in" : ""),
+              children: [
+                (0, s.jsx)("a", {
+                  className: "skip",
+                  href: "#join",
+                  children: "Skip to the form",
+                }),
+                (0, s.jsx)("header", {
+                  className: "shell top",
+                  children: (0, s.jsx)("a", {
+                    className: "logo",
+                    href: "/",
+                    "aria-label": "DEED home",
+                    children: (0, s.jsx)(i, {}),
+                  }),
+                }),
+                (0, s.jsx)("main", {
+                  className: "shell stage".concat(N ? " joined" : ""),
+                  id: "stage",
+                  children: (0, s.jsxs)("section", {
+                    className: "copy",
+                    id: "join",
+                    children: [
+                      (0, s.jsxs)("span", {
+                        className: "status",
+                        children: [
+                          (0, s.jsx)("i", { "aria-hidden": "true" }),
+                          (0, s.jsx)("span", { children: "Opening soon" }),
+                        ],
+                      }),
+                      (0, s.jsxs)("h1", {
+                        children: [
+                          "Rent is due.",
+                          (0, s.jsx)("br", {}),
+                          (0, s.jsx)("span", {
+                            style: { whiteSpace: "nowrap" },
+                            children: "Shouldn’t you get a cut?",
+                          }),
+                        ],
+                      }),
+                      (0, s.jsxs)("h2", {
+                        className: "lead",
+                        children: [
+                          "Tenants pay someone every month.",
+                          (0, s.jsx)("br", {}),
+                          "Soon, that someone is you.",
+                        ],
+                      }),
+                      !N &&
+                        (0, s.jsxs)("form", {
+                          className: "form",
+                          id: "form",
+                          onSubmit: w,
+                          noValidate: !0,
+                          children: [
+                            (0, s.jsxs)("div", {
+                              className: "row",
+                              children: [
+                                (0, s.jsx)("input", {
+                                  ref: v,
+                                  type: "email",
+                                  id: "email",
+                                  name: "email",
+                                  value: u,
+                                  onChange: (L) => {
+                                    x(L.target.value), j(null);
+                                  },
+                                  placeholder: "you@email.com",
+                                  autoComplete: "email",
+                                  inputMode: "email",
+                                  "aria-label": "Email address",
+                                  "aria-describedby": "msg",
+                                  "aria-invalid": p ? "true" : "false",
+                                  required: !0,
+                                }),
+                                (0, s.jsx)("input", {
+                                  ref: y,
+                                  className: "hp",
+                                  type: "text",
+                                  name: "company",
+                                  tabIndex: -1,
+                                  autoComplete: "off",
+                                  "aria-hidden": "true",
+                                }),
+                                (0, s.jsxs)("button", {
+                                  className: "btn",
+                                  type: "submit",
+                                  id: "go",
+                                  disabled: g,
+                                  children: [
+                                    (0, s.jsx)("span", {
+                                      id: "goLabel",
+                                      children: g
+                                        ? "ADDING YOU"
+                                        : "JOIN THE WAITLIST",
+                                    }),
+                                    (0, s.jsx)("span", {
+                                      className: "sq",
+                                      children: g
+                                        ? (0, s.jsx)("span", {
+                                            className: "spin",
+                                          })
+                                        : (0, s.jsx)(n, {}),
+                                    }),
+                                  ],
+                                }),
+                              ],
+                            }),
+                            (0, s.jsx)("p", {
+                              className: "msg",
+                              id: "msg",
+                              role: "alert",
+                              hidden: !p,
+                              children: p,
+                            }),
+                          ],
+                        }),
+                      !N &&
+                        (0, s.jsxs)("a", {
+                          className: "x-link",
+                          href: e,
+                          target: "_blank",
+                          rel: "noopener",
+                          children: [(0, s.jsx)(c, {}), "Follow on X"],
+                        }),
+                      N &&
+                        (0, s.jsxs)("div", {
+                          className: "done swap-in",
+                          id: "done",
+                          children: [
+                            (0, s.jsx)("h2", { children: "You are in." }),
+                            (0, s.jsxs)("p", {
+                              children: [
+                                "Saved ",
+                                (0, s.jsx)("b", {
+                                  id: "doneEmail",
+                                  children: N.email,
+                                }),
+                                ". First word goes out on Telegram.",
+                              ],
+                            }),
+                            (0, s.jsxs)("div", {
+                              className: "acts",
+                              children: [
+                                E &&
+                                  (0, s.jsxs)("a", {
+                                    className: "btn lime",
+                                    id: "tg",
+                                    href: E,
+                                    target: "_blank",
+                                    rel: "noopener",
+                                    children: [
+                                      "JOIN THE TELEGRAM",
+                                      (0, s.jsx)("span", {
+                                        className: "sq",
+                                        children: (0, s.jsx)(o, {}),
+                                      }),
+                                    ],
+                                  }),
+                                (0, s.jsxs)("a", {
+                                  className: "btn ghost",
+                                  href: e,
+                                  target: "_blank",
+                                  rel: "noopener",
+                                  children: [
+                                    "FOLLOW ON X",
+                                    (0, s.jsx)("span", {
+                                      className: "sq",
+                                      children: (0, s.jsx)(c, {}),
+                                    }),
+                                  ],
+                                }),
+                              ],
+                            }),
+                          ],
+                        }),
+                    ],
+                  }),
+                }),
+                (0, s.jsx)("footer", {
+                  className: "shell foot",
+                  children: (0, s.jsx)("p", { children: "\xa9 2026 DEED" }),
+                }),
+              ],
+            }),
+          ],
+        });
+      }
+    },
+  },
+  (L) => {
+    L.O(0, [441, 255, 358], () => L((L.s = 185))), (_N_E = L.O());
+  },
+]);
